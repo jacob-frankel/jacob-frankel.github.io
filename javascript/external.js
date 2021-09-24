a1ed163383d3261448434b3b4457958af7845adf
jacob-frankel <frankeldankel4@gmail.com>
2021-09-24 10:45:04 -0400

    Completed task 3

--- javascript/external.js
+++ javascript/external.js
@@ -1,8 +1,8 @@
-//document.write("Where is my JS code?");
+//const x = "web";
+//const y = "mapping";
+//const out = x + y;
+//document.write(out);
 
-//window.alert(5 + 6);
-
-//window.alert('5 + 6')
-
-document.write('<h1>A heading</h1>');
-document.write('<p>A sentence.</p>');
+//A prompt box is used to prompt users to input a value before entering a page.
+user_name = window.prompt("Please enter your name", "Type your name here");
+document.write(user_name);
