33d4ef3234252a5774a04456f3ce020c4e546ef9
jacob-frankel <frankeldankel4@gmail.com>
2021-09-24 10:46:55 -0400

    completed task 4. Operators

--- javascript/external.js
+++ javascript/external.js
@@ -4,5 +4,11 @@
 //document.write(out);
 
 //A prompt box is used to prompt users to input a value before entering a page.
-user_name = window.prompt("Please enter your name", "Type your name here");
-document.write(user_name);
+//user_name = window.prompt("Please enter your name", "Type your name here");
+//document.write(user_name);
+
+x = 22;
+const y = 33;
+document.writeln(x + y);
+document.writeln("<br>");
+document.writeln(x += 33);
