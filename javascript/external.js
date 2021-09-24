844f47dfdccee68979d90a494bfe3021aca25b42
jacob-frankel <frankeldankel4@gmail.com>
2021-09-24 11:22:14 -0400

    Uploaded image of myself

--- javascript/external.js
+++ javascript/external.js
@@ -7,8 +7,25 @@
 //user_name = window.prompt("Please enter your name", "Type your name here");
 //document.write(user_name);
 
-x = 22;
-const y = 33;
-document.writeln(x + y);
-document.writeln("<br>");
-document.writeln(x += 33);
+//x = 22;
+//const y = 33;
+//document.writeln(x + y);
+//document.writeln("<br>");
+//document.writeln(x += 33);
+
+//document.writeln("<button onclick='condition()'>Conditional Test</button>")
+//function condition() {
+  //x = confirm("Are you sure you want to proceed?");
+  //if(x){
+    //document.writeln("You choose Okay!");
+  //}
+  //else{
+    //document.writeln("You choose Cancel!");
+  //}
+//}
+
+
+arr = ["Joe","April","Mark","Sara"];
+for (var i=0; i<arr.length; i++){
+  document.writeln(arr[i],"<br>");
+}
