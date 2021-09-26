25f165038831c0ac4d17448112cceb55bd47b467
jacob-frankel <frankeldankel4@gmail.com>
2021-09-26 17:49:09 -0400

    Completed Task 6: Array

--- javascript/external.js
+++ javascript/external.js
@@ -12,14 +12,20 @@
 //document.writeln(x + y);
 //document.writeln("<br>");
 //document.writeln(x += 33);
+//
+// document.writeln("<button onclick='condition()'>Conditional Test</button>")
+// function condition() {
+//   x = confirm("Are you sure you want to proceed?");
+//   if(x){
+//     document.writeln("You choose Okay!");
+//   }
+//   else{
+//     document.writeln("You choose Cancel!");
+//   }
+// }
+//
 
-document.writeln("<button onclick='condition()'>Conditional Test</button>")
-function condition() {
-  x = confirm("Are you sure you want to proceed?");
-  if(x){
-    document.writeln("You choose Okay!");
-  }
-  else{
-    document.writeln("You choose Cancel!");
-  }
+arr = ["Joe", "April", "Mark", "Sara"];
+for (var i=0; i < arr.length; i++){
+  document.writeln(arr[i],"<br>");
 }
